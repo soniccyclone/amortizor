2de760ff71321991c8b383ecd3974d241fa6f14e
Nathan Barlow <nathanjbarlow@gmail.com>
2020-05-12 20:09:26 -0500

    Person > Make surplus payments towards a loan rollover to other loans

--- AmortizorModel/AmortizorModel/Person.cs
+++ AmortizorModel/AmortizorModel/Person.cs
@@ -16,10 +16,7 @@ namespace AmortizorModel
         private DateTime CurrentDate;
         //private decimal Salary;
 
-        public Person(IList<Loan> loans,
-            DateTime startDate,
-            decimal extraLoanRepayment,
-            bool debtSnowball)
+        public Person(IList<Loan> loans, DateTime startDate, decimal extraLoanRepayment, bool debtSnowball)
         {
             Loans = loans;
             //AnnualRaisePercent = annualRaisePercent;
@@ -37,15 +34,15 @@ namespace AmortizorModel
                 while (TotalDebt > 0)
                 {
                     var nextDate = CurrentDate.AddMonths(1);
-                    var days = (nextDate - CurrentDate).Days;
+                    var daysInCurrentMonth = (nextDate - CurrentDate).Days;
                     //Grab the information on the extra payment for the month and which loan to apply it to that
                     //month here so that we don't let any of this logic change in the middle of the month
-                    var extraPaymentLoanForMonth = ExtraPaymentLoan(days);
+                    var extraPaymentLoanForMonth = ExtraPaymentLoan(daysInCurrentMonth);
                     var extraLoanPaymentForMonth = ExtraLoanPayment;
                     //We only want to consider loans that haven't already been paid off
                     foreach (Loan loan in ApplicableLoans)
                     {
-                        var newAccruedInterest = loan.GetAccruedInterest(days);
+                        var newAccruedInterest = loan.GetAccruedInterest(daysInCurrentMonth);
                         newAccruedInterest -= loan.MinimumMonthlyPayment;
                         if (loan.Name == extraPaymentLoanForMonth.Name)
                             newAccruedInterest -= extraLoanPaymentForMonth;
@@ -55,10 +52,9 @@ namespace AmortizorModel
                         if (loan.AccruedInterest < 0)
                         {
                             loan.PrincipalBalance += loan.AccruedInterest;
-                            //TODO: Make things smarter so leftover extra payments that would have gone towards these loans will go towards other loans
+                            RolloverLoanPayment(loan.PrincipalBalance, daysInCurrentMonth);
                             loan.AccruedInterest = 0;
                         }
-                        Debug.WriteLine($"Date: {nextDate}, Loan: {loan.Name}, Principal: {loan.PrincipalBalance}");
                     }
                     CurrentDate = nextDate;
                 }
@@ -74,10 +70,23 @@ namespace AmortizorModel
         private Loan ExtraPaymentLoan(int days)
         {
             if (DebtSnowball)
-                return ApplicableLoans.OrderByDescending(l => l.PrincipalBalance).ThenBy(l => l.Name).First();
+                return ApplicableLoans.OrderBy(l => l.PrincipalBalance).ThenBy(l => l.Name).First();
             else
                 //For minimizing interest paid, we want to always put the extra payment towards wichever loan will accrue the most interest next
                 return ApplicableLoans.OrderByDescending(l => l.GetAccruedInterest(days)).ThenBy(l => l.Name).First();
+        }
+
+        private void RolloverLoanPayment(decimal loanNegativePrincipal, int days)
+        {
+            var leftoverPayment = loanNegativePrincipal;
+            while (ApplicableLoans.Any() && leftoverPayment < 0)
+            {
+                //While we have surplus payments from paid off loans, apply them to whatever is the loan we want to pay extra towards in the moment
+                var loanToPayTowards = ExtraPaymentLoan(days);
+                var newPrincipalBalance = loanToPayTowards.PrincipalBalance + leftoverPayment;
+                leftoverPayment = newPrincipalBalance;
+                loanToPayTowards.PrincipalBalance = newPrincipalBalance;
+            }
         }
     }
 }
--- AmortizorModel/AmortizorModelTests/TestPerson.cs
+++ AmortizorModel/AmortizorModelTests/TestPerson.cs
@@ -121,5 +121,31 @@ namespace AmortizorModelTests
 
             Assert.AreEqual(startDate.AddMonths(5), model.FreedomDate);
         }
+
+        [TestMethod]
+        public void Test_FreedomDate_RolloverPayment()
+        {
+            var loans = new Loan[] {
+                new Loan() {
+                    AccruedInterest = 0,
+                    PrincipalBalance = 50,
+                    InterestRate = 0.0m,
+                    MinimumMonthlyPayment = 25m,
+                    Name = "a"
+                },
+                new Loan() {
+                    AccruedInterest = 0,
+                    PrincipalBalance = 25m,
+                    InterestRate = 0.0m,
+                    MinimumMonthlyPayment = 25m,
+                    Name = "b"
+                }
+            };
+            var startDate = new DateTime(2020, 1, 1);
+
+            var model = new Person(loans, startDate, 25, true);
+
+            Assert.AreEqual(startDate.AddMonths(1), model.FreedomDate);
+        }
     }
 }
