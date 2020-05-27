e86100594bab9fa11ef58640ad2802b1fc18f9c3
Nathan Barlow <nathanjbarlow@gmail.com>
2020-05-27 07:44:34 -0500

    Add Salary model

---
+++ AmortizorModel/AmortizorModel/Models/Salary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AmortizorModel.Models
+{
+    public class Salary
+    {
+        public decimal AnnualAmount { get; set; }
+        public DateTime AnnualRaiseDate { get; set; }
+        public decimal AnnualRaisePercent { get; set; }
+    }
+}
