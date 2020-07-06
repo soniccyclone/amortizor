1b6ba96d6d31724de50c383592f6eabba664909b
Nathan Barlow <nathanjbarlow@gmail.com>
2020-07-06 08:57:21 -0500

    DebtCalendar > Create MonthlyDecisionsModel and use that to return debt repayment plan from model

--- AmortizorModel/AmortizorModel/Services/DebtCalendar.cs
+++ AmortizorModel/AmortizorModel/Services/DebtCalendar.cs
@@ -1,6 +1,8 @@
 ﻿using AmortizorModel.Interfaces;
 using AmortizorModel.Models;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -13,22 +15,28 @@ namespace AmortizorModel.Services
             Person = person;
         }
 
-        public DateTime FreedomDate(DateTime startDate)
+        public IList<MonthlyDecisionsModel> GenerateDebtRepaymentPlan(DateTime startDate)
         {
             var currentDate = startDate;
+            var monthlyDecisions = new List<MonthlyDecisionsModel>();
             while (Person.TotalDebt > 0)
             {
-                currentDate = ProcessMonth(currentDate);
+                var nextDate = currentDate.AddMonths(1);
+                monthlyDecisions.Add(new MonthlyDecisionsModel() {
+                    Month = currentDate,
+                    Decisions = ProcessMonth(currentDate, nextDate)
+                });
+                currentDate = nextDate;
             }
-            return currentDate;
+            return monthlyDecisions;
         }
 
         //This function represents the thought process a person would go through within a month to decide how to pay off their debts most efficiently
         //Long term goal would be to clean this up by using more math, but abstracting the process this way lets me avoid lots of stuff I'd have to
         //handle otherwise (e.g. leap years, rounding errors, incredibly complex differential equations)
-        private DateTime ProcessMonth(DateTime currentDate)
+        private IList<DebtDecisionModel> ProcessMonth(DateTime currentDate, DateTime nextDate)
         {
-            var nextDate = currentDate.AddMonths(1);
+            var decisions = new List<DebtDecisionModel>();
             var daysInCurrentMonth = (nextDate - currentDate).Days;
             //Grab the information on the extra payment for the month and which loan to apply it to for
             //that month here so that we don't let any of this logic change in the middle of the month
@@ -39,20 +47,20 @@ namespace AmortizorModel.Services
             //We only want to consider loans that haven't already been paid off
             foreach (ILoan loan in Person.ApplicableLoans)
             {
-                ProcessLoan(loan, extraPaymentLoanForMonth, daysInCurrentMonth, extraLoanPaymentForMonth);
-                //TODO: Change this debug line into some kind of LoanInstruction function that logs what the user should do to this loan in this month
-                Debug.WriteLine($"Date: {nextDate}, Loan: {loan.Name}, Principal: {loan.PrincipalBalance}");
+                var loanDecision = new DebtDecisionModel { LoanName = loan.Name, CurrentPrincipal = loan.PrincipalBalance };
+                loanDecision.CurrentPayment = ProcessLoan(loan, extraPaymentLoanForMonth, daysInCurrentMonth, extraLoanPaymentForMonth);
+                decisions.Add(loanDecision);
             }
-            return nextDate;
+            return decisions;
         }
 
-        private void ProcessLoan(ILoan loan, ILoan extraPaymentLoanForMonth, int daysInCurrentMonth, decimal extraLoanPaymentForMonth)
+        private decimal ProcessLoan(ILoan loan, ILoan extraPaymentLoanForMonth, int daysInCurrentMonth, decimal extraLoanPaymentForMonth)
         {
             var newAccruedInterest = loan.GetAccruedInterest(daysInCurrentMonth);
-            newAccruedInterest -= loan.MinimumMonthlyPayment;
+            var currentPayment = loan.MinimumMonthlyPayment;
             if (loan.Name == extraPaymentLoanForMonth.Name)
-                newAccruedInterest -= extraLoanPaymentForMonth;
-            loan.AccruedInterest += newAccruedInterest;
+                currentPayment += extraLoanPaymentForMonth;
+            loan.AccruedInterest += newAccruedInterest - currentPayment;
 
             //Only apply payment to principal balance if we have paid off all of our accrued interest
             if (loan.AccruedInterest < 0)
@@ -61,6 +69,8 @@ namespace AmortizorModel.Services
                 ProcessSurplusLoanPayment(loan.PrincipalBalance);
                 loan.AccruedInterest = 0;
             }
+
+            return currentPayment;
         }
 
         private void ProcessSurplusLoanPayment(decimal loanNegativePrincipal)
--- AmortizorModel/AmortizorModelTests/ServiceTests/TestDebtCalendar.cs
+++ AmortizorModel/AmortizorModelTests/ServiceTests/TestDebtCalendar.cs
@@ -2,14 +2,16 @@ using AmortizorModel.Models;
 using AmortizorModel.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace AmortizorModelTests
 {
     [TestClass]
     public class TestDebtCalendar
     {
+        //TODO: Make sure this is actually a robust test suite (need a more direct test for interest rate?)
         [TestMethod]
-        public void Test_FreedomDate_NoExtraPayment()
+        public void Test_GenerateDebtRepaymentPlan_NoExtraPayment()
         {
             var loans = new Loan[] {
                 new Loan() {
@@ -25,12 +27,13 @@ namespace AmortizorModelTests
 
             var model = new Person(loans, 0, salary);
             var service = new DebtCalendar(model);
+            var result = service.GenerateDebtRepaymentPlan(startDate);
 
-            Assert.AreEqual(startDate.AddMonths(120), service.FreedomDate(startDate));
+            Assert.AreEqual(startDate.AddMonths(119), result.Last().Month);
         }
 
         [TestMethod]
-        public void Test_FreedomDate_ExtraPayment()
+        public void Test_GenerateDebtRepaymentPlan_ExtraPayment()
         {
             var loans = new Loan[] {
                 new Loan() {
@@ -46,12 +49,13 @@ namespace AmortizorModelTests
 
             var model = new Person(loans, 25, salary);
             var service = new DebtCalendar(model);
+            var result = service.GenerateDebtRepaymentPlan(startDate);
 
-            Assert.AreEqual(startDate.AddMonths(107), service.FreedomDate(startDate));
+            Assert.AreEqual(startDate.AddMonths(106), result.Last().Month);
         }
 
         [TestMethod]
-        public void Test_FreedomDate_ExtraPaymentLoan()
+        public void Test_GenerateDebtRepaymentPlan_ExtraPaymentLoan()
         {
             var loans = new Loan[] {
                 new Loan() {
@@ -74,12 +78,13 @@ namespace AmortizorModelTests
 
             var model = new Person(loans, 25, salary);
             var service = new DebtCalendar(model);
+            var result = service.GenerateDebtRepaymentPlan(startDate);
 
-            Assert.AreEqual(startDate.AddMonths(6), service.FreedomDate(startDate));
+            Assert.AreEqual(startDate.AddMonths(5), result.Last().Month);
         }
 
         [TestMethod]
-        public void Test_FreedomDate_ExtraPaymentLoan_MinimumPaymentRollover()
+        public void Test_GenerateDebtRepaymentPlan_ExtraPaymentLoan_MinimumPaymentRollover()
         {
             var loans = new Loan[] {
                 new Loan() {
@@ -102,12 +107,13 @@ namespace AmortizorModelTests
 
             var model = new Person(loans, 25, salary);
             var service = new DebtCalendar(model);
+            var result = service.GenerateDebtRepaymentPlan(startDate);
 
-            Assert.AreEqual(startDate.AddMonths(5), service.FreedomDate(startDate));
+            Assert.AreEqual(startDate.AddMonths(4), result.Last().Month);
         }
 
         [TestMethod]
-        public void Test_FreedomDate_ProcessSurplusLoanPayment()
+        public void Test_GenerateDebtRepaymentPlan_ProcessSurplusLoanPayment()
         {
             var loans = new Loan[] {
                 new Loan() {
@@ -130,12 +136,13 @@ namespace AmortizorModelTests
 
             var model = new Person(loans, 25, salary);
             var service = new DebtCalendar(model);
+            var result = service.GenerateDebtRepaymentPlan(startDate);
 
-            Assert.AreEqual(startDate.AddMonths(1), service.FreedomDate(startDate));
+            Assert.AreEqual(startDate, result.Last().Month);
         }
 
         [TestMethod]
-        public void Test_FreedomDate_ApplyRaise()
+        public void Test_GenerateDebtRepaymentPlan_ApplyRaise()
         {
             var loans = new Loan[] {
                 new Loan() {
@@ -159,8 +166,61 @@ namespace AmortizorModelTests
 
             var model = new Person(loans, 25, salary);
             var service = new DebtCalendar(model);
+            var result = service.GenerateDebtRepaymentPlan(startDate);
 
-            Assert.AreEqual(startDate.AddMonths(6), service.FreedomDate(startDate));
+            Assert.AreEqual(startDate.AddMonths(5), result.Last().Month);
+        }
+
+        [TestMethod]
+        public void Test_GenerateDebtRepaymentPlan_Decisions()
+        {
+            var loans = new Loan[] {
+                new Loan() {
+                    AccruedInterest = 0,
+                    PrincipalBalance = 100,
+                    InterestRate = 0.0m,
+                    MinimumMonthlyPayment = 25m,
+                    Name = "a"
+                },
+                new Loan() {
+                    AccruedInterest = 0,
+                    PrincipalBalance = 100,
+                    InterestRate = 0.0m,
+                    MinimumMonthlyPayment = 25m,
+                    Name = "b"
+                }
+            };
+
+            var startDate = new DateTime(2020, 1, 1);
+
+            var salary = new Salary();
+
+            var model = new Person(loans, 25, salary);
+            var service = new DebtCalendar(model);
+            var result = service.GenerateDebtRepaymentPlan(startDate);
+
+            Assert.AreEqual(3, result.Count);
+
+            Assert.AreEqual(startDate, result[0].Month);
+            Assert.AreEqual(2, result[0].Decisions.Count);
+            AssertDecision(result[0].Decisions[0], "a", 100, 50);
+            AssertDecision(result[0].Decisions[1], "b", 100, 25);
+
+            Assert.AreEqual(startDate.AddMonths(1), result[1].Month);
+            Assert.AreEqual(2, result[1].Decisions.Count);
+            AssertDecision(result[1].Decisions[0], "a", 50, 50);
+            AssertDecision(result[1].Decisions[1], "b", 75, 25);
+
+            Assert.AreEqual(startDate.AddMonths(2), result[2].Month);
+            Assert.AreEqual(1, result[2].Decisions.Count);
+            AssertDecision(result[2].Decisions[0], "b", 50, 75);
+        }
+
+        private void AssertDecision(DebtDecisionModel decision, string loanName, decimal principal, decimal payment)
+        {
+            Assert.AreEqual(loanName, decision.LoanName);
+            Assert.AreEqual(principal, decision.CurrentPrincipal);
+            Assert.AreEqual(payment, decision.CurrentPayment);
         }
     }
 }
